9d05469f31cf36141173ca11bba1bdd7cdd8188b
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-28 19:50:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,4 +21,4 @@ function walkHome(dogName, dogBreed) {
 function unleashDog(dogName, dogBreed) {
   return `Unleash ${dogName} the ${dogBreed}`
 }
-let routine=[]
+let routine=[  wakeDog, leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
